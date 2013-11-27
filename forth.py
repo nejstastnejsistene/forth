c27d0067773f87203b34b37f48e5f4b6f1008037
Peter Johnson <pajohnson@email.wm.edu>
2013-11-26 23:46:01 -0500

    Lesson 2: Comments, including mechanism for handling delimited syntax things, and basic builtin functions.

--- forth.py
+++ forth.py
@@ -5,12 +5,58 @@ import sys
 
 class Forth(object):
 
+    def __init__(self):
+        self.stack = []
+        self.delimiter = None
+
     def push(self, word):
-        print('word:', word)
+        '''Process a single word.'''
+
+        # If the delimiter is reached, process the accumulated arguments.
+        if word == self.delimiter:
+            self.delimiter = None
+            self.action(self)
+
+        # Accumulate arguments while searching for delimiter.
+        elif self.delimiter:
+            self.args.append(word)
+
+        # Apply builtin functions.
+        elif word in builtins:
+            builtins[word](self)
+
+        # Anything else is assumed to be a base 10 integer.
+        else:
+            self.stack.append(int(word))
+
+
+    def parse_until(self, word, action=lambda f: None):
+        '''Set a delimiter, and a function to apply to parsed wordsi.'''
+        self.delimiter = word
+        self.args = []
+        self.action= action
+
+    def clear(self):
+        '''Clear end of line comments.'''
+        if self.delimiter == '\n':
+            self.delimiter = None
+
+builtins = {
+    '(': lambda f: f.parse_until(')'),
+    '\\': lambda f: f.parse_until('\n'),
+    '.': lambda f: print(f.stack.pop()),
+}
+
+
+
+def prompt(ps1='>>> '):
+    sys.stdout.write(ps1)
+    sys.stdout.flush()
 
 
 forth = Forth()
 
+prompt()
 word = ''
 while True:
     ch = sys.stdin.read(1)
@@ -20,5 +66,8 @@ while True:
             word = ''
         if not ch:
             break
+        if ch == '\n':
+            forth.clear()
+            prompt()
     else:
         word += ch
